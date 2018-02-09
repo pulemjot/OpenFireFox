a592426264416676cc2471e616bc855ecc5a73a0
pulemjot <anny.vasiljeva@gmail.com>
2018-02-09 23:10:15 +0200

    Add project files.

---
+++ OpenFireFox/Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+
+    public class Class1
+    {
+        [Test]
+        public void Run()
+        {
+            IWebDriver driver = new FirefoxDriver(@"C:\");
+            driver.Navigate().GoToUrl("http://javaguru.lv");
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+
+    }
+}
